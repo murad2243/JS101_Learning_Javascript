30fe7f3e6b395b2e9ce9df6957bb1f4aab536452
murad2243 <muradkodalwad360@gmail.com>
2022-09-05 21:42:58 +0530

    introduction to JS

---
+++ introduction_to_JS.js
@@ -0,0 +1,18 @@
+//print Masai School and A transformation inEduction in next line
+
+
+console.log("Masai School");
+
+console.log("A Transformation in Education");
+
+var name;
+
+var age;
+
+name="murad";
+
+age=19;
+
+console.log(name,age);
+
+console.log(typeof(name),typeof(age));
