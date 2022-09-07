596fb4993031e31d02c30cb120d7a76d579a50a0
murad2243 <muradkodalwad360@gmail.com>
2022-09-07 22:20:31 +0530

    Mathmetical Operation

---
+++ Math_operations.js
@@ -0,0 +1,13 @@
+// addintion of 5 numbers
+let one =1;
+let two = 2;
+let three = 3;
+let four = 4;
+let five = 5;
+console.log(one*two*three*four*five);
+
+// multiple of 3
+number = 30;
+if(number%3==0){
+  console.log("multiple of 3");
+}
